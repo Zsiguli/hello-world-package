275d06f345c34cb47aeb8a745ec42871d69fb75b
zsiguli <markovichzsigmond@gmail.com>
2017-07-18 13:03:29 +0200

    feat(Greeting): can greet specified person or just say hy

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,10 @@
+public class Greeting {
+
+  public void greetSomeone(String name) {
+    System.out.println("Hello " + name + "!");
+  }
+
+  public void greet() {
+    System.out.println("Hy!");
+  }
+}
